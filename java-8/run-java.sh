bc9fcaad3a04730389420a3dc50dd29d51283409
David Steinsland <github@davidsteinsland.net>
2017-12-18 13:06:46 +0100

    Inject truststore if available

--- java-8/run-java.sh
+++ java-8/run-java.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 set -x
 
+if test -r "${NAV_TRUSTSTORE_PATH}" -a -n "${NAV_TRUSTSTORE_PASSWORD}";
+then
+    JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.trustStore=${NAV_TRUSTSTORE_PATH} -Djavax.net.ssl.trustStorePassword=${NAV_TRUSTSTORE_PASSWORD}"
+fi
+
 exec java ${DEFAULT_JAVA_OPTS} ${JAVA_OPTS} -jar app.jar ${RUNTIME_OPTS} $@
