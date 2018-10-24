navikt/baseimages

commit f267e825d3d865f2babe582180ea877c56bc1d71

Added dumb-init to the java-8 image and as entrypoint to run-java.sh. Fixes #2

java-8/Dockerfile
@@ -1,5 +1,8 @@
 FROM openjdk:8-jre-alpine
 
+RUN wget -O /dumb-init https://github.com/Yelp/dumb-init/releases/download/v1.2.1/dumb-init_1.2.1_amd64
+RUN chmod +x /dumb-init
+
 ENV LC_ALL="no_NB.UTF-8"
 ENV LANG="no_NB.UTF-8"
 ENV TZ="Europe/Oslo"
@@ -13,4 +16,5 @@ WORKDIR /app
 
 EXPOSE 8080
 
-ENTRYPOINT ["/run-java.sh"]
+ENTRYPOINT ["/dumb-init"]
+CMD ["/run-java.sh"]

commit cee9e9798ff53f424a5e6db2a4a02bf8b7c2be7c

Fixed passing of runtime args after regression introduced in f267e825d3d865f2babe582180ea877c56bc1d71

CODEOWNERS
@@ -1,2 +1,2 @@
-* @navikt/aura
-/node-express/ @navikt/team-soknad
+*               @navikt/baseimage
+/node-express/  @navikt/team-soknad

java-10/Dockerfile
@@ -23,5 +23,4 @@ WORKDIR /app
 
 EXPOSE 8080
 
-ENTRYPOINT ["/dumb-init"]
-CMD ["/entrypoint.sh"]
+ENTRYPOINT ["/dumb-init", "--", "/entrypoint.sh"]

java-10/entrypoint.sh
@@ -11,4 +11,4 @@ else
     echo "/init-scripts does not exist, skipping startup scripts"
 fi
 
-exec /run-java.sh
+exec /run-java.sh $@

java-11/Dockerfile
@@ -20,5 +20,4 @@ WORKDIR /app
 
 EXPOSE 8080
 
-ENTRYPOINT ["/dumb-init"]
-CMD ["/entrypoint.sh"]
+ENTRYPOINT ["/dumb-init", "--", "/entrypoint.sh"]

java-11/entrypoint.sh
@@ -11,4 +11,4 @@ else
     echo "/init-scripts does not exist, skipping startup scripts"
 fi
 
-exec /run-java.sh
+exec /run-java.sh $@

java-8/Dockerfile
@@ -23,5 +23,4 @@ WORKDIR /app
 
 EXPOSE 8080
 
-ENTRYPOINT ["/dumb-init"]
-CMD ["/entrypoint.sh"]
+ENTRYPOINT ["/dumb-init", "--", "/entrypoint.sh"]

java-8/entrypoint.sh
@@ -11,4 +11,4 @@ else
     echo "/init-scripts does not exist, skipping startup scripts"
 fi
 
-exec /run-java.sh
+exec /run-java.sh $@