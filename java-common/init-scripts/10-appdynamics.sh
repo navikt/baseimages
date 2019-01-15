0a9fe93dab9d1286a6b6e86e07058fded0c52804
André Roaldseth <andre@roaldseth.net>
2019-01-15 14:56:31 +0100

    Use correct path to AppDynamics agent.jar

--- java-common/init-scripts/10-appdynamics.sh
+++ java-common/init-scripts/10-appdynamics.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-if [ -r "/opt/appdynamics/agent.jar" ] && [ -n "${APP_NAME}" ]
+if [ -r "/opt/appdynamics/javaagent.jar" ] && [ -n "${APP_NAME}" ]
 then
     APPD_NAME="${APPD_NAME:-$APP_NAME}"
     APPD_TIER="${APPD_TIER:-$APPD_NAME}"
 
-    JAVA_OPTS="${JAVA_OPTS} -javaagent:/opt/appdynamics/agent.jar"
+    JAVA_OPTS="${JAVA_OPTS} -javaagent:/opt/appdynamics/javaagent.jar"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.applicationName=${APPD_NAME}"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.nodeName=prod-${HOSTNAME}"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.tierName=prod-${APPD_TIER}"
