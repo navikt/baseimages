b61adac56911d98515d8c60f3c13773d92f8ac52
André Roaldseth <andre@roaldseth.net>
2019-01-31 07:55:06 +0100

    Reuse node names in AppDynamics

--- java-common/init-scripts/10-appdynamics.sh
+++ java-common/init-scripts/10-appdynamics.sh
@@ -12,5 +12,8 @@ then
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.applicationName=${APPD_NAME}"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.nodeName=${APPD_HOSTNAME}"
     JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.tierName=${APPD_TIER}"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName=true"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.reuse.nodeName.prefix=${APPD_NAME}"
+    JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
     export JAVA_OPTS
 fi
