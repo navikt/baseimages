bd204e03585cb6ecdae4e688ec2dcca24575e08a
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-12-20 15:18:14 +0100

    Use -storepass instead of reading from stdin
    This avoids printing of "Enter keystore password" to stderr during startup of containers.

--- java-common/init-scripts/00-truststore.sh
+++ java-common/init-scripts/00-truststore.sh
@@ -2,7 +2,7 @@
 
 if test -r "${NAV_TRUSTSTORE_PATH}";
 then
-    if ! echo "${NAV_TRUSTSTORE_PASSWORD}" | keytool -list -keystore ${NAV_TRUSTSTORE_PATH} > /dev/null;
+    if ! keytool -list -keystore ${NAV_TRUSTSTORE_PATH} -storepass "${NAV_TRUSTSTORE_PASSWORD}" > /dev/null;
     then
         echo Truststore is corrupt, or bad password
         exit 1
