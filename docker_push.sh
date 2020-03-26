588241cce6c92f42b08a99ea797811494550911e
Kyrre Havik Eriksen <kyrremann@gmail.com>
2020-03-26 08:21:46 +0100

    Bygger fint uten java10-appdynamics
    Revert "Midlertidig fjernet java10 appdynamis"
    
    This reverts commit 26415161172b7598329aa5ba9f2e1d6e426fadf0.

--- docker_push.sh
+++ docker_push.sh
@@ -5,6 +5,7 @@ docker push navikt/java:common
 docker push navikt/java:8
 docker push navikt/java:8-appdynamics
 docker push navikt/java:10
+docker push navikt/java:10-appdynamics
 docker push navikt/java:11
 docker push navikt/java:11-appdynamics
 docker push navikt/java:12
