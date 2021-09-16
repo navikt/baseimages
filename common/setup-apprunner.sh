ca9c793072688e690438a6b7373b6fcfa4021b94
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2021-09-16 10:24:47 +0200

    Make setup-apprunner.sh support older alpine

--- common/setup-apprunner.sh
+++ common/setup-apprunner.sh
@@ -11,11 +11,11 @@ mkdir -p "${home_dir}"
 
 if [ "${ID}" = "alpine" ]; then
   echo "Adding user and group with addgroup/adduser on alpine"
-  addgroup -S --gid 1069 apprunner
-  adduser --system --uid 1069 --ingroup apprunner --home "${home_dir}" apprunner
+  addgroup -S -g 1069 apprunner
+  adduser -S -u 1069 -G apprunner -h "${home_dir}" apprunner
 elif [ "$(command -v groupadd)" ]; then
   echo "Adding user and group with groupadd/useradd"
-  groupadd -S --gid 1069 apprunner
+  groupadd --system --gid 1069 apprunner
   useradd --system --home-dir "${home_dir}" --uid 1069 --gid apprunner apprunner
 else
   echo "Don't know how to add user/group!!!"
