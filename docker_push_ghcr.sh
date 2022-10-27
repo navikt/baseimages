398e0721a9b26dbae05634009e03d11d1577b0d2
albrektsson <emil.albrektsson@nav.no>
2022-10-27 10:39:52 +0200

    Removed navikt-prefix in docker_push_ghcr.sh

--- docker_push_ghcr.sh
+++ docker_push_ghcr.sh
@@ -2,7 +2,7 @@
 echo "$GITHUB_TOKEN" | docker login -u "$GITHUB_USERNAME" --password-stdin ghcr.io
 
 for img in temurin; do
-  docker push --all-tags navikt/$img
+  docker push --all-tags $img
 done
 
 docker logout ghcr.io
