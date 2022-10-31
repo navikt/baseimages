270f421384d1449cfbd8b8065e2b38abb0fb4ac0
albrektsson <emil.albrektsson@nav.no>
2022-10-31 10:20:35 +0100

    Replaced ghcr-script with workflow actions

--- docker_push_ghcr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "$GITHUB_TOKEN" | docker login -u "$GITHUB_USERNAME" --password-stdin ghcr.io
-
-for img in temurin; do
-  docker push --all-tags $img
-done
-
-docker logout ghcr.io
