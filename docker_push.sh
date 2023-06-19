c77d1998ab1c70778ab8cebe70704a74dc28fac3
albrektsson <emil.albrektsson@nav.no>
2023-06-19 10:35:07 +0200

    Replaced makefile with GHA workflow

--- docker_push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
-for img in java wildfly node-express python common; do
-  docker push --all-tags navikt/$img
-done
-
-docker logout
